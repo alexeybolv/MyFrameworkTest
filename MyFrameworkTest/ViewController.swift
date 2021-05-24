070dbaf5dea6563aa277f4239876195a5e4d0a47
alexeybolv <alexeybolv@gmail.com>
2021-05-24 19:51:06 +0300

    feat: upload test project

---
+++ MyFrameworkTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFrameworkTest
+//
+//  Created by Alexey Bolvonovich on 24.05.21.
+//
+
+import UIKit
+
+import MyFramework
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '14.5'
+
+inhibit_all_warnings!
+use_frameworks!
+
+target 'MyFrameworkTest' do
+
+pod 'MyFramework', :git => "https://github.com/alexeybolv/MyFramework.git"
+
+end
